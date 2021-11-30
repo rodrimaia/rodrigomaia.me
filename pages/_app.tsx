8dd9e6f401d702fead58ad318b5a5bc6e13a0bff
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2021-11-29 21:15:04 -0300

    :tada: hello rodrigomaia.me

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,133 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+//import '../styles/globals.css'
+import "@fontsource/open-sans/500.css";
+import {
+  Box,
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  IconButton,
+  Link,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  HamburgerIcon,
+  ExternalLinkIcon,
+  RepeatIcon,
+  EditIcon,
+} from "@chakra-ui/icons";
+
+const customTheme = {
+  fonts: {
+    heading: "Open Sans",
+    body: "Open Sans",
+  },
+  colors: {
+    haiti: "#161130",
+    lightGray: "#EBECF8",
+    blue: "#42C6FF",
+    pink: "#DF85FF",
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  },
+};
+
+const linksTopbar = [
+  { href: "/", label: "home" },
+  { href: "/resume.pdf", label: "resume", isExternal: true },
+  { href: "http://github.com/rodrimaia", label: "github", isExternal: true },
+  {
+    href: "http://linkedin.com/in/rodrimaia",
+    label: "linkedin",
+    isExternal: true,
+  },
+];
+
+const TextLink = ({ label }: { label: string }) => (
+  <Text fontSize="2xl" letterSpacing="widest" fontWeight="semibold">
+    {label}
+  </Text>
+);
+
+const Layout: React.FC = ({ children }) => {
+  const links = linksTopbar.map((l) => (
+    <NextLink key={l.href} href={l.href} passHref>
+      <Link isExternal={l.isExternal}>
+        <TextLink label={l.label} />
+      </Link>
+    </NextLink>
+  ));
+  return (
+    <Box as="body" bg="haiti" minHeight="100vh" textColor={"lightGray"}>
+      <Head>
+        <title>Rodrigo Maia</title>
+        <meta name="description" content="Rodrigo's personal site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Flex pl="7" pt="5" justify="space-between">
+          <Flex
+            visibility={{ base: "hidden", md: "visible" }}
+            sx={{ gap: "24px" }}
+          >
+            {links}
+          </Flex>
+          <Box display={{ base: "block", md: "none" }} pr="7">
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+              />
+              <MenuList>
+                {linksTopbar.map((l) => (
+                  <MenuItem key={l.href}>
+                    <NextLink key={l.href} href={l.href} passHref>
+                      <Link isExternal={l.isExternal}>
+                        <Text
+                          fontSize="2xl"
+                          color="haiti"
+                          letterSpacing="widest"
+                          fontWeight="semibold"
+                        >
+                          {l.label}
+                        </Text>
+                      </Link>
+                    </NextLink>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </Box>
+        </Flex>
+        {children}
+      </main>
+
+      <footer></footer>
+    </Box>
+  );
+};
+
+const theme = extendTheme(customTheme);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ pages/pix.tsx
@@ -0,0 +1,10 @@
+import { Box, Container, Flex, Heading, Link, Text } from "@chakra-ui/react";
+
+const Pix = () => (
+	<Container>
+	<Heading> Meu Pix:</Heading>
+	<Text>email: rodrigo.maia.pereira@gmail.com</Text>
+	</Container>
+)
+
+export default Pix;
