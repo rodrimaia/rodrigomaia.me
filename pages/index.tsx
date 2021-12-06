6afb488fc2a8f95fd77d80aa04d9680d1e950671
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2021-12-06 09:53:14 -0300

    :lipstick: reduce padding on header for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Container fontSize={["5xl", "6xl"]} pl={["8", "24"]} pt="40">
+    <Container fontSize={["5xl", "6xl"]} pl={["8", "24"]} pt={["16","40"]}>
       Hi, I&apos;m <br />
       <Text as="span" color="pink" fontWeight="bold">Rodrigo Maia</Text>, <br />
       a <Text as="span" fontWeight="bold" color="blue" fontStyle="italic">full stack developer</Text>.
