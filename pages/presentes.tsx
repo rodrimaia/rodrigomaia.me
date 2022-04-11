0a60e833f1a94ba16482c3c626da78d0dc6aa6c6
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2022-04-11 10:08:20 -0300

    :coffee: update presentes

--- pages/presentes.tsx
+++ pages/presentes.tsx
@@ -7,20 +7,8 @@ const usesSections: LinksListSection[] = [
     title: "Livros",
     items: [
       {
-        name: "The Bar Book! Elements of Cocktail Technique",
-        link: "https://www.amazon.com.br/Bar-Book-Elements-Cocktail-Technique/dp/145211384X/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr=",
-      },
-      {
-        name: "O Jogo Interior do Tênis: O Guia Clássico para o lado mental da Excelência no Desempenho",
-        link: "https://www.amazon.com.br/Interior-T%C3%AAnis-Cl%C3%A1ssico-Excel%C3%AAncia-Desempenho/dp/8569371012/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1639072873&sr=1-4",
-      },
-      {
         name: "Liquid Intelligence: The Art and Science of the Perfect Cocktail!",
         link: "https://www.amazon.com.br/Liquid-Intelligence-Science-Perfect-Cocktail/dp/0393089037/ref=sr_1_17?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=cocktails&qid=1639070775&sr=8-17&ufe=app_do%3Aamzn1.fos.4bb5663b-6f7d-4772-84fa-7c7f565ec65b",
-      },
-      {
-        name: "Imbibe!",
-        link: "https://www.amazon.com.br/Imbibe-Updated-Revised-Absinthe-Professor/dp/0399172610/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=&sr=",
       },
       {
         name: "Session Cocktails: Low-Alcohol Drinks for Any Occasion",
@@ -31,6 +19,9 @@ const usesSections: LinksListSection[] = [
   {
     title: "Outros",
     items: [
+      {
+        name: "Tabuleiro de xadrez"
+      },
       {
         name: "Mixing Glass!",
         link: "https://produto.mercadolivre.com.br/MLB-2018259069-copo-mixing-glass-720ml-timeless-bartender-coquetel-bar-_JM?matt_tool=40168140&matt_word=&matt_source=google&matt_campaign_id=14302215507&matt_ad_group_id=134553697108&matt_match_type=&matt_network=g&matt_device=c&matt_creative=539425477636&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=109453460&matt_product_id=MLB2018259069&matt_product_partition_id=1403983331189&matt_target_id=pla-1403983331189&gclid=CjwKCAiA78aNBhAlEiwA7B76p6MfqC4X2LGBuwVChj9Ut8pF74P9N6W3EoYIg0zraHP68rWI7mU5bBoCAIEQAvD_BwE",
@@ -49,12 +40,8 @@ const usesSections: LinksListSection[] = [
       },
       {
         name: "Joias",
-        link: "https://www.google.com/search?sxsrf=AOaemvLzt4M7CUujLBkcAvC_IkcrUVpBNA:1640026011643&q=joias+bonitas+mas+que+nao+me+deixam+parecendo+o+johnny+depp&spell=1&sa=X&ved=2ahUKEwj8iuL-hPP0AhV-LLkGHcfqDtkQBSgAegQIARA2&biw=1344&bih=679&dpr=2"
+        link: "https://www.google.com/search?sxsrf=AOaemvLzt4M7CUujLBkcAvC_IkcrUVpBNA:1640026011643&q=joias+bonitas+mas+que+nao+me+deixam+parecendo+o+johnny+depp&spell=1&sa=X&ved=2ahUKEwj8iuL-hPP0AhV-LLkGHcfqDtkQBSgAegQIARA2&biw=1344&bih=679&dpr=2",
       },
-      {
-        name: "Panelas e vasilhames que dá pra lavar no lava-louças",
-        link: "https://www.cleanipedia.com/br/limpeza-da-cozinha/lava-loucas-lava-panelas-de-quais-tipos.html"
-      }
     ],
   },
 ];
